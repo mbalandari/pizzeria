d60ba0b740924931c5427cc646a4fd7e744e7100
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-17 11:31:11 +0300

    add navbar items

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-export default function Layout({ Children }) {
+export default function Layout({ children }) {
     return (
         <>
             <Navbar />
-            {Children}
+            {children}
             <Footer />
         </>
     )
