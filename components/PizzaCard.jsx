9fde844e949f5b0f3a68862144cfcb213b8c99a5
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-20 12:59:24 +0300

    add axios and products

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/PizzaCard.module.css'
 
-export default function PizzaCard() {
+export default function PizzaCard({ pizza }) {
     return (
         <div className={styles.container}>
-            <Image src="/img/pizza.png" alt="pizza" width="500px" height="500px" />
-            <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-            <span className={styles.price}>$19.90</span>
-            <p className={styles.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, sed.</p>
+            <Link href={`product/${pizza._id}`} passHref>
+                <Image src={pizza.img} alt="pizza" width="500px" height="500px" />
+            </Link>
+            <h1 className={styles.title}>{pizza.title}</h1>
+            <span className={styles.price}>${pizza.prices[0]}</span>
+            <p className={styles.desc}>{pizza.desc}</p>
         </div>
     )
 }
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,7 +7,14 @@ export default async function handler(req, res) {
 
     dbConnect();
 
-    if (method === "GET") { }
+    if (method === "GET") {
+        try {
+            const products = await Product.find();
+            res.status(200).json(products);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
 
     if (method === "POST") {
         try {
